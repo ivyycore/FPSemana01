027fd9ce65d81e6e0ccda9f5e7781e59531d23e0
Maria Sequeira <mariasqueira.dev@gmail.com>
2024-11-09 02:19:37 +0000

    My Second Commit and updated characters.py

--- characters.py
+++ characters.py
@@ -18,10 +18,21 @@ p2=[name2,(atq2,def2)]
 p3=[name3,(atq3,def3)]
 plist=[p1,p2,p3]
 
-matq=max(plist)
-mdef=max(plist)
-
 print(plist)
-print(matq)
-print(mdef)
+
+if (atq1 < atq3 and atq2 < atq3):
+    print("Ataque "+name3+" "+str(atq3))
+elif (atq2 < atq1 and atq3 < atq1):
+    print("Ataque "+name1+" "+str(atq1))
+elif (atq1 < atq2 and atq3 < atq2):
+    print("Ataque "+name2+" "+str(atq2))
+
+if (def1 < def3 and def2 < def3):
+    print("Defesa "+name3+" "+str(def3))
+elif (def2 < def1 and def3 < def1):
+    print("Defesa "+name1+" "+str(def1))
+elif (def1 < def2 and def3 < def2):
+    print("Ataque "+name2+" "+str(def2))
+
+
 
